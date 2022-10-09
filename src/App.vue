f7d50d603de363423e7044758e155af4f30f969a
Md. Abdul Owhab <owhab.cse@gmail.com>
2022-10-09 22:01:56 +0600

    created counter using vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
-<script setup>
+<script>
+import Header from './components/Header/Header.vue';
+
+
+export default{
+  
+  name: 'Owhab',
+  components: Header,
+  data(){
+    return{
+      count: 0,
+    }
+
+  },
+
+}
 
 </script>
 
 <template>
-    <div>
+
+      <Header/>
+   
       <h1>Hello World</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit tenetur cum voluptates optio sequi quod odit, adipisci non, dolor sint neque assumenda nam quis voluptatibus qui aliquam est, minus quas!</p>
-    </div>
+      <h2>Counter {{count}} </h2>
+      <button @click="count++">Increment</button>
+      <button @click="count--">Decrement</button>
+    
 
 </template>
 
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,16 @@
+<script>
+    export default{
+        name: "Header Component",
+        data(){
+            
+        },
+
+    }
+</script>
+<template>
+    <h1>This is Header</h1>
+</template>
+<style scoped>
+
+</style>
+
